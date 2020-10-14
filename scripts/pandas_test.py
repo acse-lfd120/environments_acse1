76d76bc16ad88b58845a2ba1111eac10057de549
Lawrence Dior <lawrencedior9@gmail.com>
2020-10-14 09:37:39 +0100

    Added scripts_pandas_test.py
    Updated requirements.txt and environment.yml accordingly.

---
+++ scripts/pandas_test.py
@@ -0,0 +1,9 @@
+from envtest import pandas_headtail
+
+import numpy as np
+import pandas as pd
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+
+print(pandas_headtail(df))
